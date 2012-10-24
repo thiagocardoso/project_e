60eff7943501c41d2f14274b1a0e01a43f96dad1
Thiago Cardoso <thiagocardoso.s@gmail.com>
2012-10-24 00:09:45 -0200

    Adicionado usuario criador em exercicios novos

--- elearning/src/main/java/com/tcc/elearning/app/controller/ExercicioController.java
+++ elearning/src/main/java/com/tcc/elearning/app/controller/ExercicioController.java
@@ -3,9 +3,11 @@ package com.tcc.elearning.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import com.tcc.elearning.app.entity.Exercicio;
+import com.tcc.elearning.app.entity.Usuario;
 import com.tcc.elearning.app.facade.ExercicioFacade;
 import com.tcc.elearning.app.repository.ExercicioRepository;
 
@@ -18,7 +20,11 @@ public class ExercicioController {
 	ExercicioRepository exercicioRepository;
 	
 	public Exercicio novo(){
-		return exercicioFacade.novoExercicio();
+		Usuario usuario = (Usuario)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		Exercicio exercicio = exercicioFacade.novoExercicio();
+		exercicio.setCriador(usuario);
+		return exercicio;
 	}
 	
 	public List<Exercicio> getExercicios(){
