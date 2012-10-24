100deef265bc9259c4ce0f061e064754bdef85ed
Thiago Cardoso <thiagocardoso.s@gmail.com>
2012-10-24 00:05:06 -0200

    Adicionado usuario criado em cursos novos

--- elearning/src/main/java/com/tcc/elearning/app/controller/CursoController.java
+++ elearning/src/main/java/com/tcc/elearning/app/controller/CursoController.java
@@ -3,9 +3,11 @@ package com.tcc.elearning.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import com.tcc.elearning.app.entity.Curso;
+import com.tcc.elearning.app.entity.Usuario;
 import com.tcc.elearning.app.facade.CursoFacade;
 import com.tcc.elearning.app.repository.CursoRepository;
 
@@ -18,7 +20,11 @@ public class CursoController {
 	CursoRepository cursoRepository;
 	
 	public Curso novo(){
-		return cursoFacade.novoCurso();
+		Usuario usuario = (Usuario)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		Curso curso = cursoFacade.novoCurso(); 
+		curso.setCriador(usuario);
+		return curso;
 	}
 	
 	public List<Curso> getCursos(){
